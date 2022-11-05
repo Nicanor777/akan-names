6a233c374c77c873366a3191e82c32e02f21f859
Derrick Nicanor <dkahora@safaricom.co.ke>
2022-11-05 12:25:53 +0300

    added conditions to check whether Male or Female and the Day of week born

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,51 @@
 function getDayOfWeek() {
-        document.getElementById("DayOfWeek").innerHTML = (((century/4) - 2 * century - 1) + ((26 * (month + 1) / 10)) + date ) % 7
-        console.log(DayOfWeek)
+        cob = document.getElementById("cob").ariaValueMax;
+        yob = document.getElementById("yob").ariaValueMax;
+        mob = document.getElementById("mob").ariaValueMax;
+        dob = document.getElementById("dob").ariaValueMax;
+        fSex = document.getElementById("female").ariaValueMax;
+        mSex = document.getElementById("male").ariaValueMax;
+
+        document.getElementById("DayOfWeek").innerHTML = (((cob/4) - 2 * cob - 1) + ((5 * yob / 4)) + ((26 * (mob + 1) / 10)) + dob ) % 7;
+
+        if (mSex == true) {
+            if (DayOfWeek == Monday) {
+                console.log
+            } else if (DayOfWeek == Tuesday) {
+                console.log
+            } else if (DayOfWeek == Wednesday) {
+                console.log
+            } else if (DayOfWeek == Thursday) {
+                console.log
+            }
+            else if (DayOfWeek == Friday) {
+                console.log
+            }
+            else if (DayOfWeek == Saturday) {
+                console.log
+            }
+            else if (DayOfWeek == Sunday) {
+                console.log
+            }
+        } else {
+            if (DayOfWeek == Monday) {
+                console.log
+            } else if (DayOfWeek == Tuesday) {
+                console.log
+            } else if (DayOfWeek == Wednesday) {
+                console.log
+            } else if (DayOfWeek == Thursday) {
+                console.log
+            }
+            else if (DayOfWeek == Friday) {
+                console.log
+            }
+            else if (DayOfWeek == Saturday) {
+                console.log
+            }
+            else if (DayOfWeek == Sunday) {
+                console.log
+            }
+        }
+        console.log(DayOfWeek);
 }
